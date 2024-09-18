a2ac9e814598f950298cdb9376b1c17e0003dc03
alexblack24 <alex.black@mail.com>
2024-09-18 12:13:17 +0200

    simple hello world graphQL

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require("express")
+const {graphqlHTTP} = require("express-graphql")
+const {GraphQLSchema, graphql, GraphQLObjectType, GraphQLString} = require("graphql")
 const app = express()
 
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "helloworld",
+    fields: () => ({
+      message: {type: GraphQLString, resolve: () => "Hello World"},
+    }),
+  }),
+})
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+)
 app.listen(5000, () => console.log("Server Running"))
